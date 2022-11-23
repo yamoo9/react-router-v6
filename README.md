3e1925456013c130638ed3a6c2ca347096212010
yamoo9 <yamoo9@naver.com>
2022-11-24 05:35:08 +0900

    init. 학습 저장소 구성

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Router v6
+
+React Router v6 학습
---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>React Router v6 학습</h1>
+      <a href="https://reactrouter.com/" target="_blank" rel="noreferrer">
+        React Router 공식 문서
+      </a>
+    </div>
+  );
+}
+
+export default App;
