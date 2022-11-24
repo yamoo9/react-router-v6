cf6525b0f00ce795b6b11bfe930fae2052baebb4
yamoo9 <yamoo9@naver.com>
2022-11-24 10:25:10 +0900

    complete. 라우트 구성

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { useRoutes } from 'react-router-dom';
 
-const Home = React.lazy(() => import('pages/Home'));
-const Books = React.lazy(() => import('pages/Books'));
-const Book = React.lazy(() => import('pages/Book'));
-const Consumers = React.lazy(() => import('pages/Consumers'));
-const Consumer = React.lazy(() => import('pages/Consumer'));
+const Home = lazy(() => import('pages/Home'));
+const Books = lazy(() => import('pages/Books'));
+const Book = lazy(() => import('pages/Book'));
+const Consumers = lazy(() => import('pages/Consumers'));
+const Consumer = lazy(() => import('pages/Consumer'));
+
+function RouteConfig() {
+  const routeConfig = useRoutes([
+    { path: '/', element: <Home /> },
+    {
+      path: '/books',
+      element: <Books />,
+      children: [
+        {
+          index: true,
+          element: <h2>도서 이름을 선택하세요.</h2>,
+        },
+        {
+          path: ':isbn',
+          element: <Book />,
+        },
+      ],
+    },
+    {
+      path: '/consumers',
+      element: <Consumers />,
+      children: [
+        {
+          index: true,
+          element: <h2>고객 이름을 선택하세요.</h2>,
+        },
+        {
+          path: ':id',
+          element: <Consumer />,
+        },
+      ],
+    },
+  ]);
+
+  return routeConfig;
+}
 
 export default function AppRoutes() {
   return (
-    <React.Suspense>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/books" element={<Books />}>
-          <Route index element={<h2>도서 이름을 선택하세요.</h2>} />
-          <Route path=":isbn" element={<Book />} />
-        </Route>
-        <Route path="/consumers" element={<Consumers />}>
-          <Route index element={<h2>고객 이름을 선택하세요.</h2>} />
-          <Route path=":id" element={<Consumer />} />
-        </Route>
-      </Routes>
-    </React.Suspense>
+    <Suspense>
+      <RouteConfig />
+    </Suspense>
   );
 }
