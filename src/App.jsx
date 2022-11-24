a78f31b19a8d3ca05f2cb54611c5414795c31626
yamoo9 <yamoo9@naver.com>
2022-11-24 10:11:11 +0900

    complete. 코드 스플리팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalNav } from 'components/GlobalNav';
 
-import Home from 'pages/Home';
-import Books from 'pages/Books';
-import Book from 'pages/Book';
-import Consumers from 'pages/Consumers';
-import Consumer from 'pages/Consumer';
+const Home = React.lazy(() => import('pages/Home'));
+const Books = React.lazy(() => import('pages/Books'));
+const Book = React.lazy(() => import('pages/Book'));
+const Consumers = React.lazy(() => import('pages/Consumers'));
+const Consumer = React.lazy(() => import('pages/Consumer'));
 
 function App() {
   return (
@@ -23,17 +24,19 @@ function App() {
 
         <div className="border" />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/books" element={<Books />}>
-            <Route index element={<h2>도서 이름을 선택하세요.</h2>} />
-            <Route path=":isbn" element={<Book />} />
-          </Route>
-          <Route path="/consumers" element={<Consumers />}>
-            <Route index element={<h2>고객 이름을 선택하세요.</h2>} />
-            <Route path=":id" element={<Consumer />} />
-          </Route>
-        </Routes>
+        <React.Suspense>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/books" element={<Books />}>
+              <Route index element={<h2>도서 이름을 선택하세요.</h2>} />
+              <Route path=":isbn" element={<Book />} />
+            </Route>
+            <Route path="/consumers" element={<Consumers />}>
+              <Route index element={<h2>고객 이름을 선택하세요.</h2>} />
+              <Route path=":id" element={<Consumer />} />
+            </Route>
+          </Routes>
+        </React.Suspense>
       </div>
     </Router>
   );
