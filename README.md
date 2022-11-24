e216265b94124cdb0d9815c38d72624eb7f33a05
yamoo9 <yamoo9@naver.com>
2022-11-24 10:30:28 +0900

    config. 라우트 와일드 카드

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# React Router v6
+# 라우트 와일드 카드(\*)
 
-React Router v6 학습
+요청이 잘못된 경로로 접근할 경우, 라우트 와일드 카드를 사용해 페이지를 찾지 못함을 사용자에게 안내해야 합니다.
+
+- [ ] NotFound 컴포넌트 작성
+- [ ] 라우트 와일드 카드를 사용해 잘못된 접근임을 사용자에게 경고
+
+| URL 경로       | 컴포넌트  |
+| -------------- | --------- |
+| /              | Home      |
+| /books         | Books     |
+| /books/:id     | Book      |
+| /consumers     | Consumers |
+| /consumers/:id | Consumer  |
+| \*             | NotFound  |
---
+++ src/AppRoutes.jsx
@@ -0,0 +1,52 @@
+import { lazy, Suspense } from 'react';
+import { useRoutes } from 'react-router-dom';
+
+const Home = lazy(() => import('pages/Home'));
+const Books = lazy(() => import('pages/Books'));
+const Book = lazy(() => import('pages/Book'));
+const Consumers = lazy(() => import('pages/Consumers'));
+const Consumer = lazy(() => import('pages/Consumer'));
+
+function RouteConfig() {
+  const routeConfig = useRoutes([
+    { path: '/', element: <Home /> },
+    {
+      path: '/books',
+      element: <Books />,
+      children: [
+        {
+          index: true,
+          element: <h2>도서 이름을 선택하세요.</h2>,
+        },
+        {
+          path: ':isbn',
+          element: <Book />,
+        },
+      ],
+    },
+    {
+      path: '/consumers',
+      element: <Consumers />,
+      children: [
+        {
+          index: true,
+          element: <h2>고객 이름을 선택하세요.</h2>,
+        },
+        {
+          path: ':id',
+          element: <Consumer />,
+        },
+      ],
+    },
+  ]);
+
+  return routeConfig;
+}
+
+export default function AppRoutes() {
+  return (
+    <Suspense>
+      <RouteConfig />
+    </Suspense>
+  );
+}
